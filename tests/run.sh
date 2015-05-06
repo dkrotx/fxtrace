61d9d33c99b7fdf3857a75f0cd12971e042f987a
Kisel Jan <jdkrot@gmail.com>
2015-05-06 14:20:54 +0300

    do not use libtool on tests
    It doesn't have --dlopen option in old distro.

--- tests/run.sh
+++ tests/run.sh
@@ -32,9 +32,14 @@ __FXTRACE=$TESTDIR/../src/fxtrace
 [[ -s $LIBDESCR ]]  || err "$LIBDESCR not found. Did you compile sources?"
 [[ -x $__FXTRACE ]] || err "$__FXTRACE not found or not executable"
 
+FXTRACE_LIB_PATH=$( find "$TESTDIR/../" -name libfxtrace.so | head -n 1 )
+[[ -n $FXTRACE_LIB_PATH ]] || err "libfxtrace.so not found. Did you compile sources?"
+
+# Do not resolve last symlink.
+# Since being launched after install, it will be used exactly this way
+export FXTRACE_LIB_PATH=$( readlink -e $( dirname "$FXTRACE_LIB_PATH" ) )/libfxtrace.so
+
 export __FXTRACE
-# override FXTRACE_LIB_PATH. Relative path will force fxtrace to use LD_LIBRARY_PATH passed by libtool(1)
-export FXTRACE_LIB_PATH=libfxtrace.so
 cd $TESTDIR
 
 trap clear_tmp EXIT
@@ -52,7 +57,7 @@ for t in $( ls T-*.sh ); do
     printf "[%2d/%-2d] %-30s" $(( ++i )) $NTESTS $t
 
     rm -rf tmp && mkdir tmp
-    ( cd tmp && libtool --mode=execute --dlopen "$LIBDESCR" bash "../$t" ) && print_ok || { print_failed; (( NFAILED++ )); }
+    ( cd tmp && bash "../$t" ) && print_ok || { print_failed; (( NFAILED++ )); }
 done
 
 echo "----------------------------------------"
